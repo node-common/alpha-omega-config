ab4c78a7e09901fdfa5416c5b94e6c100f294770
Anton Repin <dire.one@gmail.com>
2018-05-02 14:19:51 -0700

    0.1.6 Config parser default config merge method

--- src/helpers/config.parser.ts
+++ src/helpers/config.parser.ts
@@ -1,5 +1,50 @@
+import {LoadedConfiguration} from "../models/loaded.configuration";
+import {StartupOptions} from "../main";
+
 export class ConfigParser {
 
+    public static fromOptions(o: any) : LoadedConfiguration {
 
+        return null;
+
+    }
+
+    public static mergeOptions(o1: StartupOptions & {[key:string] : any},
+                               o2: StartupOptions & {[key:string] : any})
+        : StartupOptions | {[key:string] : any} {
+
+
+        for(const key in o1) {
+
+            if(!o1.hasOwnProperty(key))
+                continue;
+
+            if(typeof o1[key] === "object") {
+
+                if(typeof o2[key] === "object") {
+
+                    o1[key] = this.mergeOptions(o1[key], o2[key]);
+
+                }
+
+            } else if(o2.hasOwnProperty(key)) {
+
+                o1[key] = o2[key];
+
+            }
+
+        }
+
+        return o1;
+
+    }
+
+    private static getCertificates(path: string) {
+
+    }
+
+    private static getConfiguration(path: string) {
+
+    }
 
 }
